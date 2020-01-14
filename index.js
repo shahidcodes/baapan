43d0e9aad02a1f47ba9ba4fb74282a548346d9f4
Shahid Kamal <shahidkamal08@gmail.com>
2020-01-14 17:00:22 +0530

    persist baapan workspace path

--- index.js
+++ index.js
@@ -10,13 +10,10 @@ import { Module } from 'module';
 import 'colors';
 
 let workspacePath = process.env.BAAPAN_WS_PATH;
-let shouldCleanup = false;
-const HOME_DIR = os.homedir();
 if (!process.env.BAAPAN_WS_PATH) {
   const WORKSPACE_DIR = `.baapan/workspace_${process.pid}_${Date.now()}`;
   workspacePath = path.join(HOME_DIR, WORKSPACE_DIR);
   process.env.BAAPAN_WS_PATH = workspacePath;
-  shouldCleanup = true;
 }
 
 const workspaceModulesDir = path.join(workspacePath, 'node_modules');
@@ -251,6 +248,7 @@ function initializeReplHistory(server) {
  * Start baapan REPL
  */
 function startRepl() {
+  console.log(workspacePath);
   switchToWorkspace(workspacePath);
   wrapRequire();
   // if history size is specified and is positive, set as max repl history size. default is 1000
